4b305bf2b95d994f30db8ecc3efa56fc1c7e9029
Gabriel Grysuk <gabrielgrysuk@gmail.com>
2025-02-16 21:55:11 -0300

    Fix Betterfox settings not applying

--- files/system/usr/lib64/firefox/defaults/pref/hardware.js
+++
@@ -1,3 +0,0 @@
-//
-pref("media.ffmpeg.vaapi.enabled", true); // Enable VAAPI because is disable by default for Nvidia
-pref("media.av1.enabled", false); // Disable AV1 support because my current hardware doesn't support hardware decoding
