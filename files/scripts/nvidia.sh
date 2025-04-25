5bd19bb5a2f2023c59c976790059f9c8f3552536
Gabriel Grysuk <gabrielgrysuk@gmail.com>
2025-04-25 13:35:12 -0300

    Use Ublue-os akmods

--- files/scripts/nvidia.sh
+++ files/scripts/nvidia.sh
@@ -3,11 +3,14 @@
 set -euoxv pipefail
 
 sed -i 's/enabled=0/enabled=1/' /etc/yum.repos.d/negativo17-*.repo
-
-curl -Lo /tmp/install-kernel-akmods.sh https://github.com/ublue-os/bluefin/blob/main/build_files/base/03-install-kernel-akmods.sh
+curl -Lo /tmp/install-kernel-akmods.sh https://raw.githubusercontent.com/ublue-os/bluefin/refs/heads/main/build_files/base/03-install-kernel-akmods.sh
 
 chmod +x /tmp/install-kernel-akmods.sh
 
-BASE_IMAGE_NAME="silverblue" UBLUE_IMAGE_TAG="42" RPMFUSION_MIRROR="" AKMODS_FLAVOR="main" KERNEL="42-6.14.2-300" /tmp/install-kernel-akmods.sh
+BASE_IMAGE_NAME="silverblue" UBLUE_IMAGE_TAG="42" RPMFUSION_MIRROR="" AKMODS_FLAVOR="main" KERNEL="6.14.2-300.fc42.x86_64" IMAGE_NAME="silverblue-nvidia" /tmp/install-kernel-akmods.sh
+
+curl -Lo /tmp/initramfs.sh https://raw.githubusercontent.com/ublue-os/bluefin/refs/heads/main/build_files/base/19-initramfs.sh
+chmod +x /tmp/initramfs.sh
+AKMODS_FLAVOR="main" /tmp/initramfs.sh
 
 sed -i 's/enabled=1/enabled=0/' /etc/yum.repos.d/negativo17-*.repo
